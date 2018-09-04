94da960ffe72fd8b0ef63222948d4dac5e1bfddb
Nicolas Massé <nicolas.masse@itix.fr>
2018-09-04 10:06:47 +0200

    initial release

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var port = 8080;
+
+router.use(function (req, res, next) {
+  next();
+  console.log("%s %s => %i", req.method, req.originalUrl, res.statusCode);
+});
+
+router.get("/location", function(req, res) {
+  var response = { "Location": "Praha" };
+  res.type('application/json')
+     .send(JSON.stringify(response))
+     .end();
+});
+
+router.get("/timeframe",function(req, res) {
+  var response = { "From": "17/09/2018", "To": "21/09/2018" };
+  res.type('application/json')
+     .send(JSON.stringify(response))
+     .end();
+});
+/*
+router.get("/participants",function(req, res) {
+  var response = [ "Manfred", "Nicolas" ];
+  res.type('application/json')
+     .send(JSON.stringify(response))
+     .end();
+});
+*/
+app.use("/",router);
+
+app.use("*",function(req, res) {
+  res.status(404).send("Not found");
+});
+
+app.listen(port, function() {
+  console.log("Live at Port %i", port);
+});
